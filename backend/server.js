6b5cfab3c769f0b0e36a1f843bff08c612c6fec1
ebramfadl <ebram@ntsal.com>
2023-09-22 15:18:52 +0300

    error handler

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const server = express();
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db")
-
+const {errorHandler} = require('./middleware/errorHandler')
 const port = process.env.PORT
 
 server.use(express.json());
@@ -14,3 +14,4 @@ server.listen(port,() => console.log(`Server is listening on port ${port}`))
 
 const goalRoutes = require('./routes/goalRoute');
 server.use('/api/goal',goalRoutes);
+server.use(errorHandler);
